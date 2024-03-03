7eaa159f4aa4df39c6b3be41112de79f998dea6f
Gülşen Bardak <gulsenbardak1@gmail.com>
2024-03-03 22:46:06 +0100

    parameter changed

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
 
   std::filesystem::path images_path = "../data/images/";
   std::filesystem::path html_path = "../webapp/result.html";
-  int size = 128;
+  int size = 12;
   int max_iter = 100;
   int query_image_index = 2;
   ipb::PlaceRecognitionApplication(images_path, size, max_iter,
